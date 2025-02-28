228dc5e2bf11160192c4c963e91a93c39dffb075
wachin <wachin.id@gmail.com>
2025-02-28 09:48:31 -0500

    update

---
+++ ejemplos de fuentes con ligaduras.py
@@ -0,0 +1,9 @@
+# Definiendo una función ficticia con palabras que contienen fi, fl, ffi, ffl
+def find_flight_info(office, affiliate):
+    office_flight = "Flight information available"
+    affiliate_flights = "Affiliate flights confirmed"
+    
+    print(office_flight)
+    print(affiliate_flights)
+
+find_flight_info("Main Office", "Global Affiliates")
